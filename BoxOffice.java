13f887cb5e5c86e624d8a772803359d24c471f50
LeoDeveci <leonardodeveci1210@gmail.com>
2026-09-01 14:41:14 -0400

    message

--- BoxOffice.java
+++ BoxOffice.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 "Leonardo Deveci, CMP129, Lab 1, 9/01/26"
 
 public class BoxOffice {
+   public static void main(string[] args)
+   {
+     Scanner keyboard = new Scanner(system)
+     string movieName;
+     int adultTicket;
+     int childTicket;
+    double Grossprofit;
+    double NetBoxOffice;
     
+
+   } 
+
     
 }
